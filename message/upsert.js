85d7d0989b0ec51f154bdb441e8a3bf57ef569d6
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-06-08 10:01:05 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -713,11 +713,7 @@ case 'play':
 await v.react('✨')
 if (!q) return v.reply('Use *' + prefix + command + ' <texto>*')
 var play = await yts(q)
-var n = 0
-if (play.all[n].type == 'channel') {
-	var n = n + 1
-}
-var vid = play.all[n]
+if (!(play.all[0] == 'channel')) { var vid = play.all[0] } else { var vid = play.all[1] }
 var teks = `\t\t\t► ${botName} Youtube
 
 ღ *Titulo:* ${vid.title}
