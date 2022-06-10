f8387ef4db0f4c1e04feb243b9372fd58dcec0fa
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-06-10 15:28:07 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -203,7 +203,7 @@ if (t == 's') { var m = 1000 } else if (t == 'm') { var m = 1000 * 60 } else if 
 await sleep(tm * m)
 var p = isGiveaways(isGiveaway(giveaway, v.chat).giveaways, senderNumber, reward).participants
 if (p.length == '0') return v.reply('Nadie ha participado en el sorteo', {mentions: groupMembers.map(x => x.id)})
-var none = Math.floor(Math.random() * p.length + 0)
+var none = Math.floor(Math.random() * p.length)
 var user = p[none]
 await v.reply('Felicidades @' + user.split('@')[0] + ' ha ganado el sorteo de *"' + reward + '"*', {mentions: groupMembers.map(x => x.id)})
 isGiveaway(giveaway, v.chat).giveaways.splice(isGiveaway(giveaway, v.chat).giveaways.indexOf(isGiveaways(isGiveaway(giveaway, v.chat).giveaways, senderNumber, reward)), 1)
